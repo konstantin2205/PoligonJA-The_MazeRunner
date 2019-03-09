da2e0e8c852e3442423ada727ed24af4fee659c8
konstantin2205 <konstantin.iv.stoyanov@gmail.com>
2019-03-09 12:18:54 +0200

    Code added

---
+++ operations_work.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+
+//prototypes
+int sum( int x , int y);
+int sub( int x , int y);
+int mup( int x , int y);
+int dev( int x , int y );
+
+int main()
+{
+	//declaring variables
+int Arg1_g;
+int Arg2_g;
+int ResultOfSumation_g;
+int ResultOfSubtraction_g;
+int ResultOfDevision_g;
+int ResultOfMultiplication_g;
+
+//printing variables
+cout<< "Arg1 = ";
+cin>>Arg1_g;
+cout<< "Arg2 = ";
+cin>>Arg2_g;
+
+//calling functions
+ResultOfSumation_g = sum( Arg1_g , Arg2_g );
+ResultOfSubtraction_g = sub( Arg1_g , Arg2_g );
+ResultOfDevision_g = mup( Arg1_g , Arg2_g );
+ResultOfMultiplication_g = dev( Arg1_g , Arg2_g );
+
+//printing  answers
+cout << "Arg1 + Arg2 = " << ResultOfSumation_g << "\r\n";
+cout << "Arg1 - Arg2 = " << ResultOfSubtraction_g << "\r\n";
+cout << "Arg1 * Arg2 = " << ResultOfDevision_g << "\r\n";
+cout << "Arg1 / Arg2 = " << ResultOfMultiplication_g << "\r\n";
+    return 0;	
+}
+//finctions 
+int sum(int x, int y)
+{
+	return(x + y);
+}
+int sub( int x, int y)
+{
+	return(x - y);
+}
+int mup( int x, int y)
+{
+	return(x * y);
+
+}
+int dev(int x , int y )
+{
+	return(x / y);
+}
